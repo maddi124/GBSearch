854b9e1ae7c3360e5f26983d8ec6ec3ce11f17e9
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-28 21:27:42 -0400

    typesDefs added

--- server/schemas/typesDefs.js
+++ server/schemas/typesDefs.js
@@ -3,8 +3,37 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        link: String
+    }
 
+    type User {
+        _id:ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
 
+    type Auth {
+        token:ID!
+        user:User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth:
+        addUser(username: String!, email: String!, password: String!): Auth:
+        saveBook():
+        removeBook():
+    }
 `;
 
 module.exports = typeDefs;
