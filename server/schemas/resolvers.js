9efebbce090440f3da0cd067b3d28c99043e5961
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-28 20:58:50 -0400

    resolvers added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,39 @@
-const { User,Book} = require('../models');
+const { User,} = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 const resolvers = {
 
-}
+    Query: {
+        me: async (parent, args, context) => {
+          if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+              .select('-__v -password')
+              .populate('Book')
+            
+        
+            return userData;
+          }
+        
+          throw new AuthenticationError('Not logged in');
+        },
+
+    },
+    Mutation:
+    {
+        login:{
+
+        },
+        addUser:{
+
+        },
+        saveBook:{
+
+        },
+        remove:{
+            
+        }
+    }
+};
 
 
 
